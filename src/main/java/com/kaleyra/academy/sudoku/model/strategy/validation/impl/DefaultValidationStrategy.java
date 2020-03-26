8a4430fcb9f0422c87c85e529d10c0f798b04edf
vinni <svinni0905@gmail.com>
2020-03-26 13:19:12 +0100

    Adding the logic

--- src/main/java/com/kaleyra/academy/sudoku/model/strategy/validation/impl/DefaultValidationStrategy.java
+++ src/main/java/com/kaleyra/academy/sudoku/model/strategy/validation/impl/DefaultValidationStrategy.java
@@ -4,44 +4,49 @@ import com.kaleyra.academy.sudoku.model.strategy.validation.SudokuValidationStra
 
 public class DefaultValidationStrategy implements SudokuValidationStrategy {
 
+	@Override
+	public boolean isValidMove(int[][] model, int row, int col, int value) {
+		return isValidForColumn(model, row, col, model[row][col]) && isValidForRow(model, row, col, model[row][col])
+				&& isValidForQuadrant(model, row, col, model[row][col]);
+	}
 
-    @Override
-    public boolean isValidMove(int[][] model, int row, int col, int value) {
-        return false;
-    }
+	public boolean isValidForQuadrant(int[][] matrix, int rowForValue, int colForValue, int value) {
+		int startI = rowForValue - rowForValue % 3;
+		int startJ = colForValue - colForValue % 3;
+		String box = "";
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) {
+				if (startI + i == rowForValue && startJ + j== colForValue)continue;
+				box += matrix[startI + i][startJ + j];
+			}
+		return !box.replaceAll("0","").contains("" + value);
+	}
 
-    /**
-     * To identify the starting point of the box the following formulas are used:
-     *
-     * - (row - row%3)  (e.g. in case of row 8 the box's top left coordinate is 6)
-     * - (col - col%3)  (e.g. in case of col 7, the box's top left corner is still 6)
-     *
-     * @param matrix
-     * @param rowForValue
-     * @param colForValue
-     * @param value
-     * @return
-     */
-    public boolean isValidForQuadrant(int[][] matrix, int rowForValue, int colForValue, int value) {
-        //TODO
-        return false;
-    }
+	public boolean isValidForRow(int[][] matrix, int rowForValue, int colForValue, int value) {
+		String row = "";
+		for (int i = 0; i < 9; i++) {
+			if (i == colForValue) continue;
+			row += matrix[rowForValue][i];
+		}
+		return !row.replaceAll("0","").contains("" + value);
+	}
 
+	public boolean isValidForColumn(int[][] matrix, int rowForValue, int colForValue, int value) {
+		
+		String col = "";
+		for (int i = 0; i < 9; i++) {
+			if (i == rowForValue) continue;
+			col += matrix[i][colForValue];
+		}
+		return !col.replaceAll("0","").contains("" + value);
+	}
 
-    public boolean isValidForRow(int[][] matrix, int rowForValue, int colForValue, int value) {
-        //TODO
-        return false;
-    }
-
-
-
-    public boolean isValidForColumn(int[][] matrix, int rowForValue, int colForValue, int value) {
-        //TODO
-        return false;
-    }
-
-    public boolean isValidModel(int[][] model) {
-        //TODO
-        return false;
-    }
+	public boolean isValidModel(int[][] model) {
+		boolean cond = true;
+		for (int i = 0; i < 9; i++)
+			for (int j = 0; j < 9; j++) {
+				cond = cond && isValidMove(model, i, j, model[i][j]);
+			}
+		return cond;
+	}
 }
