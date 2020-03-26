e6a999025537231cf798710110365612411b7904
Daniele Commodaro <daniele_ommodaro@live.it>
2020-03-26 15:52:20 +0100

    test secondo step

--- src/main/java/com/kaleyra/academy/sudoku/model/strategy/validation/impl/DefaultValidationStrategy.java
+++ src/main/java/com/kaleyra/academy/sudoku/model/strategy/validation/impl/DefaultValidationStrategy.java
@@ -24,12 +24,70 @@ public class DefaultValidationStrategy implements SudokuValidationStrategy {
      */
     public boolean isValidForQuadrant(int[][] matrix, int rowForValue, int colForValue, int value) {
         //TODO
+        int x = 0;
+        int y = 0;
+
+        if(rowForValue < 3 && colForValue < 3)
+        {
+            x = 0;
+            y = 0;
+        }
+        else if(rowForValue < 3 && colForValue > 2 && colForValue < 6)
+        {
+            x = 0;
+            y = 3;
+        }
+        else if(rowForValue < 3 && colForValue > 5)
+        {
+            x = 0;
+            y = 6;
+        }
+        else if(rowForValue < 6 && rowForValue > 2 && colForValue < 3)
+        {
+            x = 3;
+            y = 0;
+        }
+        else if(rowForValue < 6 && rowForValue > 2 && colForValue > 2 && colForValue < 6)
+        {
+            x = 3;
+            y = 3;
+        }
+        else if(rowForValue < 6 && rowForValue > 2 && colForValue > 5)
+        {
+            x = 3;
+            y = 6;
+        }
+        else if(rowForValue > 6 && colForValue < 3)
+        {
+            x = 6;
+            y = 0;
+        }
+        else if(rowForValue > 5 && colForValue > 2 && colForValue < 6)
+        {
+            x = 6;
+            y = 3;
+        }
+        else if(rowForValue > 5 && colForValue > 5)
+        {
+            x = 6;
+            y = 6;
+        }
+
+        for(int i = x; i < x + 2; i++)
+            for(int j = y; j < y + 2; j++)
+                if(matrix[i][j] == value)
+                {
+                    //System.out.println("entrato con: " + matrix[i][j]);
+                    return true;
+                }
+
         return false;
     }
 
 
     public boolean isValidForRow(int[][] matrix, int rowForValue, int colForValue, int value) {
         //TODO
+
         if(value == 0)
             value = -1;
 
@@ -54,7 +112,20 @@ public class DefaultValidationStrategy implements SudokuValidationStrategy {
     }
 
     public boolean isValidModel(int[][] model) {
-        //TODO
-        return false;
+
+        boolean ret = false;
+        boolean invalid = false;
+
+        if(model.length >= 8 && model != null && !invalid)
+            ret = true;
+
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++)
+                if(model[i][j] < 0 || model[i][j] > 8)
+                {
+                    ret = false;
+                    invalid = true;
+                }
+        return ret;
     }
 }
