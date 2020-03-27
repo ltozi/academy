9d5b6b6efa1c3b6a47b6b47a677e9164b2f7b291
Daniele Commodaro <daniele_ommodaro@live.it>
2020-03-27 14:10:26 +0100

    step 4 completato

--- src/main/java/com/kaleyra/academy/sudoku/model/strategy/generation/impl/SimpleGenerationStrategy.java
+++ src/main/java/com/kaleyra/academy/sudoku/model/strategy/generation/impl/SimpleGenerationStrategy.java
@@ -12,10 +12,21 @@ public class SimpleGenerationStrategy extends RandomNewGameStrategy {
 
         Random random = new Random();
         int[][] data = model.getData();
+        int DIFFICULTY_LEVEL = 0;
+
+        System.setProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile", "hard");
+
+        String property =
+                System.getProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile");
+
+        switch (property) {
+            case "hard":
+                DIFFICULTY_LEVEL = 3;
+        }
 
         for (int rowIndex = 0; rowIndex < data.length; rowIndex++) {
             for (int colIndex = 0; colIndex < data.length; colIndex++) {
-                int emptyValue = random.nextInt(LEVEL_MEDIUM);
+                int emptyValue = random.nextInt(DIFFICULTY_LEVEL);
                 if(emptyValue == 0) {
                     data[rowIndex][colIndex] = 0; //remove from generated
                 }
