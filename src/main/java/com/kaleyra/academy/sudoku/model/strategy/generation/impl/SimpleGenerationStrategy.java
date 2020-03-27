02ed6fc9c71d064892155add46954e4286be571f
Daniele Commodaro <daniele_ommodaro@live.it>
2020-03-27 14:38:16 +0100

    step 4 completato

--- src/main/java/com/kaleyra/academy/sudoku/model/strategy/generation/impl/SimpleGenerationStrategy.java
+++ src/main/java/com/kaleyra/academy/sudoku/model/strategy/generation/impl/SimpleGenerationStrategy.java
@@ -10,18 +10,29 @@ public class SimpleGenerationStrategy extends RandomNewGameStrategy {
     @Override
     public GameModel setGameDifficulty(GameModel model) {
 
+        String property = "";
         Random random = new Random();
         int[][] data = model.getData();
         int DIFFICULTY_LEVEL = 0;
 
-        System.setProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile", "hard");
+        //scrive la proprietà
+        System.setProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile", "easy/medium/hard");
 
-        String property =
-                System.getProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile");
+        //legge la proprietà
+        String a[] = System.getProperty("com.kaleyra.academy.sudoku.model.ChangeGameDifficultyManager.useFile").split
+                ("/", 3);
+
+        property = a[2];
 
         switch (property) {
             case "hard":
-                DIFFICULTY_LEVEL = 3;
+                DIFFICULTY_LEVEL = LEVEL_DIFFICULT;
+                break;
+            case "medium":
+                DIFFICULTY_LEVEL = LEVEL_MEDIUM;
+                break;
+            case "easy":
+                DIFFICULTY_LEVEL = LEVEL_EASY;
         }
 
         for (int rowIndex = 0; rowIndex < data.length; rowIndex++) {
